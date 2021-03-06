1661a6ee79a1b62e027eeb20697bff8dc20c4c03
randycv <43799126+randycv@users.noreply.github.com>
2021-03-06 18:03:02 -0500

    Move different nodes with text box
    Nodes are able to move around and the text box stays with the node.

--- createMap.js
+++ createMap.js
@@ -1,5 +1,5 @@
-CANVAS_WIDTH =700;
-CANVAS_HEIGHT = 700;
+CANVAS_WIDTH =1920;
+CANVAS_HEIGHT = 1080;
 
 // Node Class
 // grabbed => indicates if the node is being 'grabbed' by the mouse
@@ -13,31 +13,57 @@ CANVAS_HEIGHT = 700;
 
 var inp;
 var numOfNodes = 1;
-var value = 1;
+var value = 0;
 var valSet = 1;
 let node = [];
+let inputs = [];
 var j;
+let px, py;
 // Creates the canvas
 
 
 function addValue() {
   value++;
+  addNode();
   console.log(value);
+}
+
+function addNode() {
+  node1 = new Node(200+(value*100),200,200,100,false);
+
+  node1.inp.input(myInputEvent);
+  node1.inp.position(150+(value*100),215);
+  node1.inp.size(100,40);
+  node1.inp.style('backround-color', color(255,255,255));
+  node1.inp.changed(textFromBox);
+
+  node.push(node1);
+  inputs.push(node1.inp);
+
+
+  draw();
+
+
+  //for(j=0; j<=value; j++){
+  //  node[j] = new Node(200+(j*20),200,200,100,false);
+  //  }
+
 }
 
 function setup() {
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   background(66, 135, 245);
-  for(j=0; j<=value; j++){
-    node[j] = new Node(200+(j*20),200,200,100,false);
-    }
 
-  inp = createInput();
+  //node1 = new Node(200+(value*20),200,200,100,false);
+  //node.push(node1);
+
+  /*inp = createInput();
   inp.input(myInputEvent);
-  inp.position(200,50);
+  inp.position(200+(value*20),200);
   inp.size(250,40);
   inp.style('backround-color', color(255,255,255));
   inp.changed(textFromBox);
+  inputs.push(inp);*/
   // Input Text Field over node
 }
 
@@ -62,28 +88,75 @@ function draw(){
   //ellipse(node.x, node.y, node.diameter, node.radius);
   textAlign(CENTER);
   //text(inp.value(), node.x, node.y);
-  show();
+  show(mouseX, mouseY);
   //  print(grabbed);
 }
 
 class Node {
-  constructor(x, y, diameter, radius, grabbed) {
+  constructor(x, y, diameter, radius, grabbed, text, resize) {
     this.x = x;
     this.y = y;
     this.diameter = diameter;
     this.radius = radius;
     this.grabbed = grabbed;
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.text = text;
+    this.inp = createInput();
+    this.resize = resize;
+  }
+
+  clicked(px, py) {
+    let d = dist(px, py, this.x , this.y);
+    if (d < this.radius) {
+      this.grabbed = true;
+      this.offsetX = this.x - px;
+      this.offsetY = this.y - py;
+    }
+    //else if(px == this.x && py == this.y){
+    //  this.resize = true;
+    //}
+
+  }
+  notClicked(px, py){
+    this.grabbed = false;
+    this.resize = false;
   }
 
 }
 
+var k;
+
+function mousePressed(){
+  for (k=0; k<node.length; k++) {
+    node[k].clicked(mouseX, mouseY);
+  }
+}
+
+function mouseReleased(){
+  for (k=0; k<node.length; k++){
+    node[k].notClicked();
+  }
+}
+
+
 var i = 0;
 
-function show() {
-  stroke(255);
+function show(px, py) {
+  stroke(51);
   strokeWeight(4);
+  //scale(mouseX / 400, mouseY / 400);
   for (i=0; i<=value-1; i++){
     ellipse(node[i].x, node[i].y, node[i].diameter, node[i].radius, node[i].grabbed);
+    if(node[i].grabbed) {
+      node[i].x = px + node[i].offsetX;
+      node[i].y = py + node[i].offsetY;
+      node[i].inp.position(px + node[i].offsetX - 50, py + node[i].offsetY+15);
+    }
+
+    if(node[i].resize) {
+      node[i].scale(mouseX / 400, mouseY / 400);
+    }
   }
 }
 
