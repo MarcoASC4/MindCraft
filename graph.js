964df7438fd29823cfd9ae165c89fe1b8921cbe8
ACHamann <andrewhamann1999@gmail.com>
2021-03-14 14:17:51 -0400

    edits to graph.js

--- graph.js
+++ graph.js
@@ -15,6 +15,7 @@ function getJSONFromURL() {
 }
 
 
+
 //create an empty graph JSON object
 function createGraph(title)
 {
@@ -37,8 +38,6 @@ function createEdge(source, target, label)
     label:  label
   };
 }
-//remove an edge from a graph JSON object
-
 
 
 //NODE Methods
@@ -54,32 +53,32 @@ function createNode(text, x, y)
 }
 
 
-//run this to see how it is created
-var g = createGraph("test graph"); // create empty graph
-console.log("Empty Graph created:");
-console.log(g);
-
-console.log("Creating Empty Node");
-var n = createNode("text label", 100, 100) //create node with given attributes
-console.log(n);
-
-console.log("Adding Node");
-g.graph.nodes["nodeName or ID"] = n; //add a node to graph            NOTE: Identical names or ids will overwrite
-console.log(g);
-
-console.log("Removing Node")
-delete g.graph.nodes["nodeName or ID"]; //remove a node
-console.log(g);
-
-//similarly
-console.log("Nodes with id1 and id2");
-g.graph.nodes["id1"] = n;
-g.graph.nodes["id2"] = n;
-
-console.log("Adding Edge");
-g.graph.edges["Edge name or ID"] = createEdge("id1", "id2", "Edge Label") //add an edge
-console.log(g);
-
-console.log("Removing Edge");
-delete g.graph.edges["Edge name or ID"]; //remove an edge
-console.log(g);
+// //run this to see how it is created
+// var g = createGraph("test graph"); // create empty graph
+// console.log("Empty Graph created:");
+// console.log(g);
+//
+// console.log("Creating Empty Node");
+// var n = createNode("text label", 100, 100) //create node with given attributes
+// console.log(n);
+//
+// console.log("Adding Node");
+// g.graph.nodes["nodeName or ID"] = n; //add a node to graph            NOTE: Identical names or ids will overwrite
+// console.log(g);
+//
+// console.log("Removing Node")
+// delete g.graph.nodes["nodeName or ID"]; //remove a node
+// console.log(g);
+//
+// //similarly
+// console.log("Nodes with id1 and id2");
+// g.graph.nodes["id1"] = n;
+// g.graph.nodes["id2"] = n;
+//
+// console.log("Adding Edge");
+// g.graph.edges["Edge name or ID"] = createEdge("id1", "id2", "Edge Label") //add an edge
+// console.log(g);
+//
+// console.log("Removing Edge");
+// delete g.graph.edges["Edge name or ID"]; //remove an edge
+// console.log(g);
